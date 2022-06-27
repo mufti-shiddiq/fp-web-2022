2035042830697c5befae803d33f06d8e501409a9
nugrohowisnu04 <nugrohowisnu@students.amikom.ac.id>
2022-06-27 23:22:39 +0700

    update halaman kontak

---
+++ layouts/head.html
@@ -0,0 +1,18 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="assets/css/style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- Font Poppins & Signika -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+  href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Signika:wght@400;500;600;700&display=swap"
+  rel="stylesheet">
+
+<!-- Font Awesome -->
+<script src="https://kit.fontawesome.com/1779ae588d.js" crossorigin="anonymous"></script>
+
+<link rel="icon" type="image/x-icon" href="assets/favicon.ico">
---
+++ produk.php
@@ -0,0 +1,217 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include "layouts/head.html" ?>
+    <title>Space Roastery</title>
+</head>
+
+<body>
+    <?php include "layouts/navbar.html" ?>
+
+    <!-- Content -->
+    <section>
+    <div class="wrap mb-5">
+    <div class="produk justify-content-center">
+        <h2 class="text-center mt-8 mb-5"><b>Katalog <span style="color:#EE6924;">Produk</span></b></h2>
+        <div class="row">
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/gayo.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> Gayo Apple Cider</h5>
+                <!-- <p>Bridge To Specialty Filter Coffee</p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/lintong.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Lintong</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/nutella.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Nutella</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/strawberry.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Strawberry</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</section>
+<section>
+    <div class="wrap mb-5">
+    <div class="produk justify-content-center">
+        <div class="row">
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/gayo.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> Gayo Apple Cider</h5>
+                <!-- <p>Bridge To Specialty Filter Coffee</p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/lintong.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Lintong</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/nutella.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Nutella</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/strawberry.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Strawberry</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</section>
+<section>
+    <div class="wrap mb-5">
+    <div class="produk justify-content-center">
+        <div class="row">
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/gayo.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> Gayo Apple Cider</h5>
+                <!-- <p>Bridge To Specialty Filter Coffee</p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/lintong.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Lintong</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/nutella.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Nutella</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="width: 15rem;">
+            <img src="assets/img/product/strawberry.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Strawberry</h5>
+                <!-- <p></p> -->
+                <a href="#" class="card-link">Rp125.000</a>
+                <a href="#" class="btn float-end btn-sm btn-primary">Detail</a>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</section>
+
+<style>
+    .center {
+  text-align: center;
+}
+.pagination {
+  display: inline-block;
+}
+
+.pagination a {
+  color: black;
+  float: left;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+.pagination a.active {
+  background-color: #EE6924;
+  color: white;
+  border-radius: 5px;
+}
+
+.pagination a:hover:not(.active) {
+  background-color: #ddd;
+  border-radius: 5px;
+}
+</style>
+<div class="center">
+<div class="pagination">
+<a href="#">&laquo;</a>
+<a href="#" class="active">1</a>
+<a href="#" >2</a>
+<a href="#">3</a>
+<a href="#">4</a>
+<a href="#">5</a>
+<a href="#">6</a>
+<a href="#">&raquo;</a>
+</div>
+</div>
+    <!-- End Content -->
+
+
+    <?php include "layouts/footer.html" ?>
+    <?php include "layouts/footer-script.html" ?>
+
+</body>
+
+</html>
