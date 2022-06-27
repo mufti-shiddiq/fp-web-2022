b0f519f05c4087c6deeb6d1a66a51db5acbda7d4
Senli <senliyusdiantoro971@students.amikom.ac.id>
2022-06-27 15:42:02 +0700

    page produk & tentang kami

---
+++ aboutus.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include "layouts/head.html" ?>
+    <title>Space Roastery</title>
+</head>
+
+<body>
+    <?php include "layouts/navbar.html" ?>
+
+    <!-- Content -->
+    <div class="container">
+    <div class="card mb-10 ">
+<img src="assets/img/about-top-image.jpg" class="rounded " alt="...">
+<div class="heading-wrap text-center">    
+    <h2 class="text-uppercase">"we are not a coffee addict. just a little over attached to it"</h2>
+</div>
+<div class="card-body ">
+    <p class="card-text">Kebiasaan kami meminum kopi sudah dimulai sejak cukup lama sejak saat kami masih menjadi pelajar di luar negeri. Setiap pagi sebelum berangkat sekolah, 
+        kami selalu menyempatkan diri untuk mengunjungi coffee shop di dekat apartemen dan memesan Americano. Jujur saja, pada saat itu kami lebih tertarik dengan arom dari kopi yang diseduh dan bau dari coffee shop, 
+        awalnya kami bahkan tidak suka dengan rasa kopi hitam yang kami pesan, tetapi aroma dan efek yang ditimbulkan oleh kopi-lah yang membuat kami selalu datang kembali. </p>
+       <br>
+       <br>
+        <p>Singkat cerita, pada waktu kami kembali lagi tinggal di Indonesia, kami menemukan kejanggalan, di negara yang merupakan penghasil salah satu penghasil kopi terbesar di dunia, kami kesusahan untuk mendapatkan kopi berkualitas yang selalu kami konsumsi pada saat kami masih di luar negeri. 
+            Kami sudah mencoba beberapa alternatif, kopi dari supermarket? Kebanyakan sudah tidak fresh. Starbucks? Terlalu hambar dan mahal. Roastery lokal? Hasilnya tidak konsisten dan kopinya susah didapatkan, yang kami dapatkan justru customer service yang buruk. Sampai ada di satu titik dimana kami akhirnya memutuskan untuk memulai roasting kopi sendiri, 
+            dengan berbekal pengalaman ngopi, deep research, dan banyak membaca, kami berhasil. Selang beberapa bulan, ternyata banyak yang menyukai kopi kami, dan beberapa dari mereka bahkan menanyakan apakah kami menjualnya. Dari situlah kami mulai meng-komersialkan kopi kami dengan nama Space Roastery. </p>
+
+</div>
+<div class="heading-wrap text-center">
+<h2 class="text-uppercase">"kami punya komitmen dimana semua kopi yang kami produksi adalah kopi yang fresh dari biji yang berkualitas"</h2>
+</div>
+</div>
+    <!-- End Content -->
+
+    <?php include "layouts/footer.html" ?>
+    <?php include "layouts/footer-script.html" ?>
+
+</body>
+
+</html>
---
+++ kontak.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include "layouts/head.html" ?>
+    <title>Space Roastery</title>
+</head>
+
+<body>
+    <?php include "layouts/navbar.html" ?>
+
+    <!-- Content -->
+<section id="kontak" style="padding: 100px 20px;">
+<div class="container">
+<h3>
+    <b>KONTAK</b>
+</h3>
+<br><br>
+<div class="row">
+    <div class="col-lg-6" style="padding-left: 50px;">
+    <h4>ALAMAT</h4>
+    <!-- <address >
+    <ul>
+        <li>
+            <span class="fa-solid fa-location-dot"></span>
+        </li>
+        <li>
+            <span>Gg.Loncang No.88, Rogoyudan, Sinduadi, kec.Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55284</span>
+        </li> -->
+        <!-- <a style="color: black; list-style: none;"><i class="fa-solid fa-location-dot"></i>&nbsp; Gg.Loncang No.88, Rogoyudan, Sinduadi, kec.Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55284</a> -->
+    <!-- </address> -->
+    <table>
+        <tr>
+    <td><span class="fa-solid fa-location-dot fa-2x">&nbsp;</span></td>
+    <br>
+    <td><span>Gg.Loncang No.88, Rogoyudan, Sinduadi, Kec.Mlati, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55284</span></td>
+        </tr>
+    </table>
+    <h4 style="margin-top: 50px">KONTAK</h4>
+    <p></p>
+    </div>
+</div>
+</div>
+
+    <!-- End Content -->
+
+    <?php include "layouts/footer.html" ?>
+    <?php include "layouts/footer-script.html" ?>
+
+</body>
+
+</html>
