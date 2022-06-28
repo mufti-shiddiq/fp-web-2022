656d9a50e98696adc3d2ff7b6b50a20eff04e9fd
Senli <senliyusdiantoro971@students.amikom.ac.id>
2022-06-28 09:59:03 +0700

    update halaman about us

--- aboutus.php
+++ aboutus.php
@@ -4,13 +4,61 @@
 <head>
     <?php include "layouts/head.html" ?>
     <title>Space Roastery</title>
+
 </head>
 
 <body>
     <?php include "layouts/navbar.html" ?>
 
+    <style>
+
+
+        .wrappertext{
+            height:100%;
+            /* margin-bottom:50px; */
+        }
+        .wrappertext p {
+            line-height:2;
+            text-align: justify;
+            margin:0;
+        }
+        .scaffold{
+            float:left;
+            height: 400px;
+            shape-outside: url(.../img/about-top-image.jpg);
+        }
+    </style>
+
     <!-- Content -->
-    <div class="container">
+<div class="container wrappertext mt-5 pt-5">
+<div class="container-fluid">
+    <img src="assets/img/slide-5.jpg" class="img-fluid" alt="">
+</div>
+<div class="heading-wrap text-center mt-5 ">    
+    <h2 class="text-uppercase">"we are not a coffee addict. just a little over attached to it"</h2>
+</div>
+    <div class="row p-5">
+        <div class="col-md">
+            
+                <img src="assets/img/about-top-image.jpg" class="img-fluid scaffold pe-3 pt-3" alt="gambar">
+                <p class="mb-2">  Kebiasaan kami meminum kopi sudah dimulai sejak cukup lama sejak saat kami masih menjadi pelajar di luar negeri. Setiap pagi sebelum berangkat sekolah, 
+        kami selalu menyempatkan diri untuk mengunjungi coffee shop di dekat apartemen dan memesan Americano. Jujur saja, pada saat itu kami lebih tertarik dengan arom dari kopi yang diseduh dan bau dari coffee shop, 
+        awalnya kami bahkan tidak suka dengan rasa kopi hitam yang kami pesan, tetapi aroma dan efek yang ditimbulkan oleh kopi-lah yang membuat kami selalu datang kembali. </p>
+       </p>
+       <p>  Singkat cerita, pada waktu kami kembali lagi tinggal di Indonesia, kami menemukan kejanggalan, di negara yang merupakan penghasil salah satu penghasil kopi terbesar di dunia, kami kesusahan untuk mendapatkan kopi berkualitas yang selalu kami konsumsi pada saat kami masih di luar negeri. 
+            Kami sudah mencoba beberapa alternatif, kopi dari supermarket? Kebanyakan sudah tidak fresh. Starbucks? Terlalu hambar dan mahal. Roastery lokal? Hasilnya tidak konsisten dan kopinya susah didapatkan, yang kami dapatkan justru customer service yang buruk. Sampai ada di satu titik dimana kami akhirnya memutuskan untuk memulai roasting kopi sendiri, 
+            dengan berbekal pengalaman ngopi, deep research, dan banyak membaca, kami berhasil. Selang beberapa bulan, ternyata banyak yang menyukai kopi kami, dan beberapa dari mereka bahkan menanyakan apakah kami menjualnya. Dari situlah kami mulai meng-komersialkan kopi kami dengan nama Space Roastery. </p>
+        </div>
+        </div>
+        <div class="heading-wrap text-center mt-5 mb-5">
+<h2 class="text-uppercase">"kami punya komitmen dimana semua kopi yang kami produksi adalah kopi yang fresh dari biji yang berkualitas"</h2>
+
+    </div>
+</div>
+
+
+
+    <!-- <div class="container">
     <div class="card mb-10 ">
 <img src="assets/img/about-top-image.jpg" class="rounded " alt="...">
 <div class="heading-wrap text-center">    
@@ -30,7 +78,8 @@
 <div class="heading-wrap text-center">
 <h2 class="text-uppercase">"kami punya komitmen dimana semua kopi yang kami produksi adalah kopi yang fresh dari biji yang berkualitas"</h2>
 </div>
-</div>
+</div> -->
+
     <!-- End Content -->
 
     <?php include "layouts/footer.html" ?>
