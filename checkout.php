c40d84c05cb45274ead6218e20caaa63cf5eafbe
yudha <romansa.yudha@students.amikom.ac.id>
2022-06-30 14:04:31 +0700

    update halaman keranjang

--- checkout.php
+++ checkout.php
@@ -9,10 +9,16 @@
    .gradient-custom {
 background: white;
 }
+.produk .card {
+    border: none;
+    border-radius: 20px;
+    box-shadow: 5px 5px 15px rgba(165, 168, 204, 0.3);
+}
     </style>
 </head>
 
-<body>
+<body onload="myFunction()">
+
     <?php include "layouts/navbar.html" ?>
     <div class="py-3"></div>
     
@@ -21,9 +27,9 @@ background: white;
   <div class="container py-5">
     <div class="row d-flex justify-content-center my-4">
       <div class="col-md-8">
-        <div class="card mb-4">
-          <div class="card-header py-3">
-            <h5 class="mb-0">Cart - 2 items</h5>
+        <div class="card mb-4" style="border-radius: 20px;">
+          <div class="card-header py-3" style="border-top-left-radius: 20px; border-top-right-radius: 20px;">
+            <h5 class="mb-0">2 Item Di Keranjang Anda</h5>
           </div>
           <div class="card-body">
             <!-- Single item -->
@@ -31,8 +37,8 @@ background: white;
               <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
                 <!-- Image -->
                 <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/12a.webp"
-                    class="w-100" alt="Blue Jeans Jacket" />
+                  <img src="assets/img/product/gayo.jpg"
+                    class="w-100" style="border-radius: 30px; padding: 20px;"/>
                   <a href="#!">
                     <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
                   </a>
@@ -42,9 +48,9 @@ background: white;
 
               <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
                 <!-- Data -->
-                <p><strong>Blue denim shirt</strong></p>
-                <p>Size: M</p>
-                <button type="button" class="btn btn-primary btn-sm me-1 mb-2" style="background: red; border-color: red;" data-mdb-toggle="tooltip"
+                <p><strong>Gayo Apple Cider</strong></p>
+                <p>Kemasan : 200g</p>
+                <button type="button" class="btn btn-secondary btn-sm me-1 mb-2" style="background: red; border-color: red;" data-mdb-toggle="tooltip"
                   title="Remove item">
                   <i class="fas fa-trash"></i>
                 </button>
@@ -54,17 +60,17 @@ background: white;
               <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
                 <!-- Quantity -->
                 <div class="d-flex mb-4" style="max-width: 300px">
-                  <button class="btn btn-secondary px-3 me-2"
+                  <button class="btn btn-secondary px-3 me-2" style="border-radius: 8px;"
                     onclick="this.parentNode.querySelector('input[type=number]').stepDown(); myFunction()">
                     <i class="fas fa-minus"></i>
                   </button>
 
                   <div class="form-outline">
                     <input id="form1" min="0" name="quantity" value="1" type="number" class="form-control" oninput="myFunction()"/>
-                    <label class="form-label" for="form1">Quantity</label>
+                    <label class="form-label" for="form1">Jumlah</label>
                   </div>
 
-                  <button class="btn btn-secondary px-3 ms-2"
+                  <button class="btn btn-secondary px-3 ms-2" style="border-radius: 8px;"
                     onclick="this.parentNode.querySelector('input[type=number]').stepUp(); myFunction()">
                     <i class="fas fa-plus"></i>
                   </button>
@@ -73,7 +79,7 @@ background: white;
 
                 <!-- Price -->
                 <p class="text-start text-md-center">
-                  <strong id="harga-item-1">100000</strong>
+                  <strong id="harga-item-1">125000</strong>
                 </p>
                 <!-- Price -->
               </div>
@@ -87,8 +93,8 @@ background: white;
               <div class="col-lg-3 col-md-12 mb-4 mb-lg-0">
                 <!-- Image -->
                 <div class="bg-image hover-overlay hover-zoom ripple rounded" data-mdb-ripple-color="light">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Vertical/13a.webp"
-                    class="w-100" />
+                  <img src="assets/img/product/lintong.jpg"
+                    class="w-100" style="border-radius: 30px; padding: 20px;"/>
                   <a href="#!">
                     <div class="mask" style="background-color: rgba(251, 251, 251, 0.2)"></div>
                   </a>
@@ -98,10 +104,10 @@ background: white;
 
               <div class="col-lg-5 col-md-6 mb-4 mb-lg-0">
                 <!-- Data -->
-                <p><strong>Red hoodie</strong></p>
-                <p>Size: M</p>
+                <p><strong>Lintong</strong></p>
+                <p>Kemasan : 200g</p>
 
-                <button type="button" class="btn btn-primary btn-sm me-1 mb-2" style="background: red; border-color: red;" data-mdb-toggle="tooltip"
+                <button type="button" class="btn btn-secondary btn-sm me-1 mb-2" style="background: red; border-color: red;" data-mdb-toggle="tooltip"
                   title="Remove item">
                   <i class="fas fa-trash"></i>
                 </button>
@@ -110,18 +116,18 @@ background: white;
 
               <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
                 <!-- Quantity -->
-                <div class="d-flex mb-4" style="max-width: 300px">
-                  <button class="btn btn-secondary px-3 me-2"
+                <div class="d-flex mb-4" style=" max-width: 300px">
+                  <button class="btn btn-secondary px-3 me-2" style="border-radius: 8px;"
                     onclick="this.parentNode.querySelector('input[type=number]').stepDown(); myFunction()">
                     <i class="fas fa-minus"></i>
                   </button>
 
                   <div class="form-outline">
                     <input id="form2" min="0" name="quantity" value="1" type="number" class="form-control" oninput="myFunction()"/>
-                    <label class="form-label" for="form2">Quantity</label>
+                    <label class="form-label" for="form2">Jumlah</label>
                   </div>
 
-                  <button class="btn btn-secondary px-3 ms-2"
+                  <button class="btn btn-secondary px-3 ms-2" style="border-radius: 8px;"
                     onclick="this.parentNode.querySelector('input[type=number]').stepUp(); myFunction()">
                     <i class="fas fa-plus"></i>
                   </button>
@@ -130,7 +136,7 @@ background: white;
 
                 <!-- Price -->
                 <p class="text-start text-md-center">
-                  <strong id="harga-item-2">100000</strong>
+                  <strong id="harga-item-2">125000</strong>
                 </p>
                 <!-- Price -->
               </div>
@@ -138,54 +144,38 @@ background: white;
             <!-- Single item -->
           </div>
         </div>
-        <div class="card mb-4 mb-lg-0">
-          <div class="card-body">
-            <p><strong>We accept</strong></p>
-            <img class="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/visa.svg"
-              alt="Visa" />
-            <img class="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/amex.svg"
-              alt="American Express" />
-            <img class="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce-gateway-stripe/assets/images/mastercard.svg"
-              alt="Mastercard" />
-            <img class="me-2" width="45px"
-              src="https://mdbcdn.b-cdn.net/wp-content/plugins/woocommerce/includes/gateways/paypal/assets/images/paypal.webp"
-              alt="PayPal acceptance mark" />
-          </div>
-        </div>
+
       </div>
       <div class="col-md-4">
-        <div class="card mb-4">
-          <div class="card-header py-3">
-            <h5 class="mb-0">Summary</h5>
+        <div class="card mb-4" style="border-radius: 20px;">
+          <div class="card-header py-3" style="border-top-left-radius: 20px; border-top-right-radius: 20px;">
+            <h5 class="mb-0">Total Pembayaran</h5>
           </div>
           <div class="card-body">
             <ul class="list-group list-group-flush">
               <li
                 class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                Products
+                Produk
                 <span id="total">0</span>
               </li>
               <li class="list-group-item d-flex justify-content-between align-items-center px-0">
-                Shipping
+                Pengiriman
                 <span>Gratis</span>
               </li>
               <li
                 class="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
                 <div>
-                  <strong>Total amount</strong>
+                  <strong>Jumlah Total</strong>
                   <strong>
-                    <p class="mb-0">(including VAT)</p>
+                    <p class="mb-0">(termasuk PPN)</p>
                   </strong>
                 </div>
                 <span id="grand-total"><strong>0</strong></span>
               </li>
             </ul>
 
-            <button type="button" class="btn btn-secondary btn-lg btn-block" style="background: #EE6924; border-color: #EE6924;">
-              Go to checkout
+            <button type="button" class="btn btn-secondary btn-lg btn-block" style="border-radius: 8px; background: #EE6924; border-color: #EE6924;">
+              Bayar Sekarang
             </button>
           </div>
         </div>
@@ -195,14 +185,13 @@ background: white;
 </section>
 <script>
     function myFunction() {
-        // var price = document.getElementById("harga-item-1").value;
-        var price = 100000;
-        var price = 100000;
+        var price1 = 125000;
+        var price2 = 125000;
 
         var jml1 = document.getElementById("form1").value;
         var jml2 = document.getElementById("form2").value;
-        var total1 = +price * +jml1;
-        var total2 = +price * +jml2;
+        var total1 = +price1 * +jml1;
+        var total2 = +price2 * +jml2;
         var total = +total1 + +total2;
 
         document.getElementById("total").innerHTML = total;
